a6cba47336b084d4e2b606bd81967cd3ebd25a1e
mohamedhanafi69120 <your-email@example.com>
2025-04-21 11:05:17 +0200

    Ajout du composant Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header";
 import Navigation from "./components/routes/Routes";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Navigation />
+      <Footer />
     </>
   );
 }
--- src/components/feature-item/FeatureItem.jsx
+++ src/components/feature-item/FeatureItem.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const FeatureItem = ({ imageSource, imageAlt, title, paragraph }) => {
   return (
     <div className="feature-item">
-      <img src={imageSource} alt={imageAlt} className="feature-icon" />
-      <h3 className="feature-title">{title}</h3>
+      <img className="feature-icon" src={imageSource} alt={imageAlt} />
+      <h2 className="sr-only">Features</h2>
+      <h3 className="feature-item-title">{title}</h3>
       <p>{paragraph}</p>
     </div>
   );
